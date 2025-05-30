304db49625fa4a008f57d061d61a6e4b246696fa
aland <aandreenko1997@gmail.com>
2025-05-30 23:56:45 +0700

    color files

---
+++ lua/colors/nvimtree.lua
@@ -0,0 +1,4 @@
+vim.api.nvim_set_hl(0, "NvimTreeCursorLine", { bg = "#11262D" })
+vim.api.nvim_set_hl(0, "NvimTreeFolderName", { fg = "#ffb100" })
+vim.api.nvim_set_hl(0, "NvimTreeOpenedFolderName", { fg = "#f9cf76" })
+vim.api.nvim_set_hl(0, "NvimTreeFolderIcon", { fg = "#c05746" })
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,7 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function ()
             require('mason-lspconfig').setup({
-                ensure_installed = { 'lua_ls', 'ts_ls', 'eslint-lsp', }
+                ensure_installed = { 'lua_ls', 'ts_ls', 'eslint-lsp', 'css-lsp'}
             })
         end
     },
